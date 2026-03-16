ee7e11f0351035feb19a927a05902869fac0f6e2
Gustavo Viana Pedro <gvpedro@stefanini.com>
2026-03-16 18:23:14 -0300

    feat: Adiciona MisoesRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,12 +1,16 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
-
 import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
